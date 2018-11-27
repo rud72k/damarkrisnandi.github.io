916fd264a606deb618a97a3a12387ecacbd01ed5
Damar Ramdan <damarkrisnandi1202@gmail.com>
2018-11-27 08:47:15 +0700

    landing page fixing align button

--- src/components/landingPageItems/landingSignup.vue
+++ src/components/landingPageItems/landingSignup.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <section class="hero is-medium is-orange" style="margin-top: 70px">
-        <div class="hero-body text">
+        <div class="hero-body text centered">
             <p class="subtitle" style="text-align: center; color: white;">Lorem ipsum dolor sit amet</p>
-            <div class="container centered">
+            <div class="container">
                 <a href="" class="button is-large is-outlined is-light">Sign Up</a>
             </div>
         </div>
@@ -19,12 +19,8 @@ export default {
 </script>
 
 <style>
-    .centered {
-        display: block;
-        margin: auto;
-    }
     .is-orange {
-        background-color: rgb(199, 65, 13);
+        background-color: rgb(243, 132, 29);
         color: white;
     }
 </style>
